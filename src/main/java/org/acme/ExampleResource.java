9711b02ec85f03ec89ae38df41345bb8cf60b07f
nadia <nadiahosseini018@gmail.com>
2025-03-16 15:36:59 +0330

    initial commit

---
+++ src/main/java/org/acme/ExampleResource.java
@@ -0,0 +1,26 @@
+package org.acme;
+
+import io.quarkus.grpc.GrpcClient;
+import io.smallrye.mutiny.Uni;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+@Path("/hello")
+public class ExampleResource {
+    @GrpcClient
+    HelloGrpc hello;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+
+    @GET
+    @Path("/{name}")
+    public Uni<String> hello(String name) {
+        return hello.sayHello(HelloRequest.newBuilder().setName(name).build())
+                .onItem().transform(HelloReply::getMessage);
+    }
+}
